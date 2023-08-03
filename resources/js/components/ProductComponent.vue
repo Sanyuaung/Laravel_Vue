1524962e5b422bf7455e2c19ede017f0877da843
SanYuAung <sanyuaung.ygn.mm@gmail.com>
2023-08-03 20:02:42 +0630

    CreatePage

---
+++ resources/js/components/ProductComponent.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="container my-5">
+        <div class="row justify-content-end mb-3">
+            <div class="col-4">
+                <button class="btn btn-primary">
+                    <i class="fas fa-plus-circle"></i> Create
+                </button>
+            </div>
+            <div class="col-4">
+                <form>
+                    <div class="input-group">
+                        <input type="text" placeholder="Search" class="form-control">
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-primary">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <div class="card">
+                    <h4 class="card-header">Create</h4>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label class="mb-2">Name: </label>
+                                <input type="text" class="form-control mb-2">
+                            </div>
+                            <div class="form-group">
+                                <label class="mb-2">Price: </label>
+                                <input type="number" class="form-control mb-2">
+                            </div>
+                            <button class="btn btn-primary">
+                                <i class="fas fa-save"></i> Save
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-8">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Book</td>
+                            <td>1000</td>
+                            <td>
+                                <button class="btn btn-success btn-sm"><i class="fas fa-edit"></i> Edit</button>
+                                <button class="btn btn-danger btn-sm"><i class="fas fa-trash-alt"></i> Delete</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    }
+}
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Larevel + Vue (SPA)</title>
-        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
     </head>
     <body>
-        <h1>Larevel + Vue (SPA)</h1>
         <div id="app">
-            <example-component></example-component>
+            <product-component></product-component>
         </div>
-        <script src="/js/app.js"></script>
+        <script src="{{ mix('/js/app.js') }}"></script>
     </body>
 </html>
